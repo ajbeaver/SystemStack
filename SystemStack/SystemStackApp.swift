a61f696d987cccc6bcc167f651c5c8567d91bd18
ajbeaver <ajbeaver@me.com>
2026-02-15 07:50:56 -0700

    Initial Commit

---
+++ SystemStack/SystemStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemStackApp.swift
+//  SystemStack
+//
+//  Created by bea on 2/15/26.
+//
+
+import SwiftUI
+
+@main
+struct SystemStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
